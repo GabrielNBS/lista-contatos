d79966014f5d5000755946d308438be529c9e84c
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-09-05 22:55:27 -0300

    configurando redux

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,20 +1,32 @@
+import { useSelector } from 'react-redux'
 import Tarefa from '../../Components/Tarefa'
 import { Container } from './styles'
 
-const ListaDeTarefas = () => (
-  <Container>
-    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;Termo&ldquo;</p>
+import { RootReducer } from '../../store'
 
-    <ul>
-      <li>
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-      </li>
-    </ul>
-  </Container>
-)
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+
+  return (
+    <Container>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;Termo&ldquo;
+      </p>
+
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              prioridade={t.prioridade}
+              status={t.status}
+              titulo={t.titulo}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState: [
+    new Tarefa(
+      'Estudar Programação',
+      enums.Prioridade.URGENTE,
+      enums.Status.PENDENTE,
+      'estudar ao menos 3 horas por dia para garantir o futuro da minha familia',
+      1
+    )
+  ],
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state = state.filter((tarefa) => tarefa.id !== action.payload)
+    }
+  }
+})
+
+export const { remover } = tarefasSlice.actions
+
+export default tarefasSlice.reducer
